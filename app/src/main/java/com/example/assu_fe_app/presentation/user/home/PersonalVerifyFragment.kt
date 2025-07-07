30961306b0daff4fb3a6d66e81728c483c034e29
eeeeeaaan <wjdwjddla73@naver.com>
2025-07-07 12:25:38 +0900

    [FEAT/#31] QR없이 인증화면 구현

---
+++ app/src/main/java/com/example/assu_fe_app/presentation/user/home/PersonalVerifyFragment.kt
@@ -0,0 +1,26 @@
+package com.example.assu_fe_app.presentation.user.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentPersonalVerifyBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+
+class PersonalVerifyFragment : BaseFragment<FragmentPersonalVerifyBinding>(R.layout.fragment_personal_verify) {
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+        // 모두 인증이 완료 되었을때 btnPersonalVerifyComplete의 색을 활성화 해줘야 됨.
+
+
+    }
+
+
+}
