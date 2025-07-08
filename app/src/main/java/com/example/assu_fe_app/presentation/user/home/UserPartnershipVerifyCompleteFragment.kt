6d9bbdbbac10315ab8f09262ecfa37157946cb49
2ghrms <aa020228@gmail.com>
2025-07-09 03:48:36 +0900

    [FEAT/#40] 제휴 완료 화면 작성

---
+++ app/src/main/java/com/example/assu_fe_app/presentation/user/home/UserPartnershipVerifyCompleteFragment.kt
@@ -0,0 +1,26 @@
+package com.example.assu_fe_app.presentation.user.home
+
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentUserPartnershipVerifyCompleteBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+class UserPartnershipVerifyCompleteFragment :
+    BaseFragment<FragmentUserPartnershipVerifyCompleteBinding>(R.layout.fragment_user_partnership_verify_complete) {
+
+    override fun initView() {
+        // X 버튼 클릭 시 홈으로 이동
+        binding.ivCross.setOnClickListener {
+            // Activity 종료 후 HomeFragment가 있는 이전 화면으로 돌아감
+            requireActivity().finish()
+        }
+
+        // (선택적으로 리뷰 작성하기 버튼 처리 등)
+        binding.btnCheckContract.setOnClickListener {
+            // 추후 연결 가능
+        }
+    }
+
+    override fun initObserver() {
+        // 옵저버 등록 필요 시 작성
+    }
+}
