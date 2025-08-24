76b30fbf3235e78c1626b4670ab40a8422390060
2ghrms <aa020228@gmail.com>
2025-08-25 04:39:17 +0900

    [FEAT/#57] 파트너 계정관리 탭 UI 추가
    - 알림설정 xml & fragment 추가

--- app/src/main/java/com/example/assu_fe_app/presentation/partner/mypage/PartnerMypageFragment.kt
+++ app/src/main/java/com/example/assu_fe_app/presentation/partner/mypage/PartnerMypageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.assu_fe_app.presentation.partner.mypage
 
 import android.content.Intent
+import androidx.navigation.fragment.findNavController
 import com.example.assu_fe_app.R
 import com.example.assu_fe_app.databinding.FragmentPartnerMypageBinding
 import com.example.assu_fe_app.presentation.base.BaseFragment
@@ -12,7 +13,7 @@ class PartnerMypageFragment : BaseFragment<FragmentPartnerMypageBinding>(R.layou
 
         // 알림 설정 창
         binding.clPartnerAccountComponent1.setOnClickListener {
-
+            findNavController().navigate(R.id.action_partner_mypage_to_alarm)
         }
 
         // 로그아웃 창
