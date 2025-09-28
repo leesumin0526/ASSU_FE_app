70ca6f1749750cdd694d2f24cb01f1a8b9bcd807
BAEK0111 <baekjw111@gmail.com>
2025-09-28 16:24:24 +0900

    fix: #83 chatting api connect - 다이얼로그 모서리 8dp로 보이도록 수정

--- app/src/main/java/com/example/assu_fe_app/BlockOpponentDialogFragment.kt
+++ app/src/main/java/com/example/assu_fe_app/BlockOpponentDialogFragment.kt
@@ -2,7 +2,13 @@ package com.example.assu_fe_app
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -63,6 +69,33 @@ class BlockOpponentDialogFragment : DialogFragment() {
         return AlertDialog.Builder(requireContext())
             .setView(view)
             .create()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val context = context ?: return
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = windowManager.currentWindowMetrics
+            val insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(android.view.WindowInsets.Type.systemBars())
+            val width = windowMetrics.bounds.width() - insets.left - insets.right
+
+            // 화면 가로 너비의 85% 정도로 설정 (원하는 비율로 조절)
+            val dialogWidth = (width * 0.85).toInt()
+            dialog?.window?.setLayout(dialogWidth, WindowManager.LayoutParams.WRAP_CONTENT)
+
+        } else {
+            val display = windowManager.defaultDisplay
+            val size = Point()
+            display.getSize(size)
+
+            // 화면 가로 너비의 85% 정도로 설정 (원하는 비율로 조절)
+            val dialogWidth = (size.x * 0.85).toInt()
+            dialog?.window?.setLayout(dialogWidth, WindowManager.LayoutParams.WRAP_CONTENT)
+        }
+        // 이 시점에는 dialog 와 window 가 확실히 존재합니다.
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 
     companion object {
